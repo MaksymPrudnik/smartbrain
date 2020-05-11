09c8b9d2fca05ee09f44e295ad98833e1a9af550
MaksymPrudnik <maksymprudnik@gmail.com>
2020-05-12 00:26:05 +0300

    init commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import brain from './brain.png';
+import './Logo.css'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+               <div className="Tilt-inner f7">
+                   <img alt='Brain' src={brain} width='100%'></img>
+                   <a target="_blank" rel="noopener noreferrer" href="https://icons8.com/icons/set/brain">Brain icon</a> icon by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a>
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
